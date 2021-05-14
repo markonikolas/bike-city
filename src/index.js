e54499e9384af76fee13e8b3e53402fb102cd9af
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-05-15 01:18:57 +0200

    Update header and hero links

--- src/index.js
+++ src/index.js
@@ -1,47 +1,52 @@
-import $ from 'jquery';
+import './stylesheets/main.sass'
+import $ from 'jquery'
 
-// Styles
-import './stylesheets/main.sass';
+$(() => {
+	function scrollToArticle(e) {
+		e.preventDefault()
+		$('body').removeClass('no-scroll')
+		$('.nav').removeClass('nav--open')
+		$('#menu').removeClass('menu--open')
+		$('#hero').removeClass('hero--active')
 
-$( () => {
-	function scrollToArticle( e ) {
-		e.preventDefault();
-		const target = e.target.hash;
-		const $target = $( target );
+		const target = e.target.hash
+		const $target = $(target)
 
-		const link = e.target.href || null;
+		const link = e.target.href || null
 
-		let $amount = $target.offset() || 0;
+		let $amount = $target.offset() || 0
 
-		if ( $amount ) {
-			$amount = $amount.top;
+		if ($amount) {
+			$amount = $amount.top
 		}
 
 		// Animate scrolling
-		$( 'html, body' )
+		$('html, body')
 			.stop()
-			.animate(
-				{
-					scrollTop: $amount
-				},
-				500,
-				'swing',
-				() => {
-					window.location.hash = target;
-					if ( link ) {
-						window.location = link;
-					}
+			.animate({ scrollTop: $amount }, 500, 'swing', () => {
+				window.location.hash = target
+				if (link) {
+					window.location = link
 				}
-			);
+			})
 	}
 
-	$( '#menu' ).on( 'click', function () {
+	$('#menu').on('click', function () {
+		const $nav = $(this).siblings('.nav')
+		const $hero = $('#hero')
+
+		$nav.toggleClass('nav--open')
+		$hero.toggleClass('hero--active')
+
 		// Toggle main navigation
-		$( this ).toggleClass( 'menu--open' );
+		$(this).toggleClass('menu--open')
 
 		// Lock body from scrolling
-		$( 'body' ).toggleClass( 'no-scroll' );
-	} );
+		$('body').toggleClass('no-scroll')
+	})
 
-	$( '#explore' ).on( 'click', e => scrollToArticle( e ) );
-} );
+	$('#explore').on('click', (e) => scrollToArticle(e))
+	$('.nav__link').on('click', (e) => scrollToArticle(e))
+
+	$('#footer-date').html(new Date().getFullYear().toString())
+})
