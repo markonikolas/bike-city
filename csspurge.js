16e26cc3143febf00a0bd90900ff4fc01a28ccd3
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-05-15 01:19:27 +0200

    Update asset generation and code linting

--- csspurge.js
+++ csspurge.js
@@ -1,6 +1,6 @@
 (async function purgeStylesWithHtml() {
-	const csspurge = require("css-purge");
-	const fs = require("fs/promises");
+	const csspurge = require('css-purge');
+	const fs = require('fs/promises');
 
 	const assetDir = `${__dirname}/public/static`;
 	const stylesDir = `${assetDir}/styles`;
@@ -15,7 +15,7 @@
 				css_output: `${stylesDir}/${style}`,
 				verbose: true,
 			},
-			"config/csspurge.config.json"
+			'config/csspurge.config.json',
 		);
 	});
 })();
